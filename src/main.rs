50e3dbb8afa4dde2356ff59a55800cc2bbfa5193
Collins Muriuki <murerwacollins@gmail.com>
2020-06-17 12:41:47 +0300

    Add loop expression

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,30 @@ fn main() {
     let secret_number = rand::thread_rng()
                                 .gen_range(1, 101);
 
-    println!("The secret number is {}", secret_number);
+    // println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // Rust variables are immutable by default
-    // The following example shows how to use 'mut' before the variable name to make a variable mutable:
-    let mut guess = String::new();
-    // The :: syntax in the ::new line indicates that new is an associated function of the String type
-    // An associated function is implemented on a type, in this case String, rather than on a particular instance of a String. Some languages call this a static method.
-    
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        // Rust variables are immutable by default
+        // The following example shows how to use 'mut' before the variable name to make a variable mutable:
+        let mut guess = String::new();
+        // The :: syntax in the ::new line indicates that new is an associated function of the String type
+        // An associated function is implemented on a type, in this case String, rather than on a particular instance of a String. Some languages call this a static method.
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
