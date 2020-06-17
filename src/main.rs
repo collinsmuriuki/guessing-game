433373380c4a05605dafcfcfca5e2aca20f1703f
Collins Muriuki <murerwacollins@gmail.com>
2020-06-17 12:29:11 +0300

    Parse string to 32bit number and compare with match expression

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng; // The Rng trait defines methods that random number generators implement
 
 // https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng()
+                                .gen_range(1, 101);
+
+    println!("The secret number is {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -16,6 +23,15 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
 
-    println!("You guessed: {}", &guess);
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
